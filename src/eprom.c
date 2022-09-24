8af37e5a7637db9d628573fe0b463b435f004e13
enscrypted <112054797+enscrypted@users.noreply.github.com>
2022-09-24 09:39:45 +0000

    Add files via upload

---
+++ src/eprom.c
@@ -0,0 +1,18 @@
+// Allows access to the board's EEPROM storage, which is used for
+// the lock combination
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <driverlib/sysctl.h>
+#include <inc/hw_memmap.h>
+#include <driverlib/gpio.h>
+#include <driverlib/pin_map.h>
+#include <driverlib/eeprom.h>
+#include "Util/launchpad.h"
+#include "eprom.h"
+
+void epromInit()
+{
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_EEPROM0);
+        EEPROMInit();
+}
