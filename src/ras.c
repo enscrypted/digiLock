54fbc2092df809373dc8080c2e05c9e860fcc861
enscrypted <112054797+enscrypted@users.noreply.github.com>
2022-09-24 09:53:02 +0000

    Update ras.c

--- src/ras.c
+++ src/ras.c
@@ -14,17 +14,17 @@
 void rasInit() {
 
 
-    //Enable the ADC0 module
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+  //Enable the ADC0 module
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
 
-    //Configure the ADC0, sequencer #0
-    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
+  //Configure the ADC0, sequencer #0
+  ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0);
 
-    //Configure the sequencer
-    ADCSequenceStepConfigure(ADC0_BASE, 0, 0,  ADC_CTL_CH7 | ADC_CTL_IE |  ADC_CTL_END );
+  //Configure the sequencer
+  ADCSequenceStepConfigure(ADC0_BASE, 0, 0,  ADC_CTL_CH7 | ADC_CTL_IE |  ADC_CTL_END );
 
-    //Enable the sequence
-    ADCSequenceEnable(ADC0_BASE, 0);
+  //Enable the sequence
+  ADCSequenceEnable(ADC0_BASE, 0);
 
 
 }
